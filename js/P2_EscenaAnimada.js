cd2a163f9a36966499be3bd34060e8d8a832a985
Blai <blaipuchol@gmail.com>
2024-02-28 17:39:52 +0100

    New scene

--- js/P2_EscenaAnimada.js
+++ js/P2_EscenaAnimada.js
@@ -5,22 +5,22 @@
  * Se trata de añadir un interfaz de usuario que permita 
  * disparar animaciones sobre los objetos de la escena con Tween
  * 
- * @author 
+ * @author <bpucsal@inf.upv.es>, 2024
  * 
  */
 
 // Modulos necesarios
-/*******************
- * TO DO: Cargar los modulos necesarios
- *******************/
+import * as THREE from "../lib/three.module.js";
+import {GLTFLoader} from "../lib/GLTFLoader.module.js";
+import {OrbitControls} from "../lib/OrbitControls.module.js";
+import {TWEEN} from "../lib/tween.module.min.js";
+import {GUI} from "../lib/lil-gui.module.min.js";
 
 // Variables de consenso
 let renderer, scene, camera;
 
 // Otras globales
-/*******************
- * TO DO: Variables globales de la aplicacion
- *******************/
+let cameraControls, effectController;
 
 // Acciones
 init();
@@ -33,9 +33,7 @@ function init()
     // Motor de render
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
-    /*******************
-    * TO DO: Completar el motor de render y el canvas
-    *******************/
+    document.getElementById('container').appendChild( renderer.domElement );
 
     // Escena
     scene = new THREE.Scene();
@@ -43,9 +41,8 @@ function init()
     // Camara
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1,1000);
     camera.position.set( 0.5, 2, 7 );
-    /*******************
-    * TO DO: Añadir manejador de camara (OrbitControls)
-    *******************/
+    cameraControls = new OrbitControls( camera, renderer.domElement );
+    cameraControls.target.set(0,1,0);
     camera.lookAt( new THREE.Vector3(0,1,0) );
 }
 
@@ -61,14 +58,23 @@ function loadScene()
 
 function loadGUI()
 {
-    // Interfaz de usuario
-    /*******************
-    * TO DO: Crear la interfaz de usuario con la libreria lil-gui.js
-    * - Funcion de disparo de animaciones. Las animaciones deben ir
-    *   encadenadas
-    * - Slider de control de radio del pentagono
-    * - Checkbox para alambrico/solido
-    *******************/
+    // Definicion de los controles
+	effectController = {
+		mensaje: 'Bosque',
+		giroY: 0.0,
+		separacion: 0,
+		colorsuelo: "rgb(150,150,150)"
+	};
+
+	// Creacion interfaz
+	const gui = new GUI();
+
+	// Construccion del menu
+	const h = gui.addFolder("Control esferaCubo");
+	h.add(effectController, "mensaje").name("Aplicacion");
+	h.add(effectController, "giroY", -180.0, 180.0, 0.025).name("Giro en Y");
+	h.add(effectController, "separacion", { 'Ninguna': 0, 'Media': 2, 'Total': 5 }).name("Separacion");
+    h.addColor(effectController, "colorsuelo").name("Color alambres");
 }
 
 function update(delta)
